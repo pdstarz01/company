0bf24774ff24646aae5807ee594864b3d163be9c
Starz <elitesorenea1@gmail.com>
2024-09-24 12:53:31 -0600

    initial commit

---
+++ pages/views.py
@@ -0,0 +1,20 @@
+from typing import Any
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+# Create your views here.
+def home_page_view(request):
+    context = {
+        "inventory_list": ["Widget 1", "Widget 2","Widget 3"], 
+        "greeting": "Thank you for Visiting.",
+    }
+    return render(request, "home.html", context)
+
+class AboutPageView(TemplateView):
+    template_name = "about.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["contact_address"] = "123 Ur Mom's House"
+        context["phone_number"] = "666-666-6666"
+        return context
